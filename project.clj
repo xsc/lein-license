590c3cd1f52970deefd9bd8d515712461d035420
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-04-16 18:20:48 +0200

    Version 0.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-license "0.1.2"
+(defproject lein-license "0.1.3-SNAPSHOT"
   :description "Project-Level License Management."
   :url "https://github.com/xsc/lein-license"
   :license {:name "MIT License"

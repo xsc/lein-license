b3167f07545297a7a852d3b0e66d95f1b260c6ac
Yannick Scherer <yannick.scherer@futurice.com>
2018-08-29 09:01:37 +0200

    Use 'lein-isolate' to isolate plugin dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
             :url "https://opensource.org/licenses/MIT"
             :key "mit"
             :year 2015}
-  :dependencies [[org.yaml/snakeyaml "1.17"]
-                 [rewrite-clj "0.5.0"
-                  :exclusions [org.clojure/clojure]]
-                 [cheshire "5.8.0"]]
+  :dependencies [^:source-dep [org.yaml/snakeyaml "1.17"]
+                 ^:source-dep [rewrite-clj "0.6.1" :exclusions [org.clojure/clojure]]
+                 ^:source-dep [cheshire "5.8.0"]]
+  :plugins [[lein-isolate "0.1.0"]]
+  :middleware [leiningen.isolate/middleware]
   :eval-in :leiningen
   :pedantic? :abort)

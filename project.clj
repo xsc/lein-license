4d462d4c4a6ffb06cf497b7380baa6b1641d06ec
Yannick Scherer <yannick.scherer@futurice.com>
2018-09-01 13:22:52 +0200

    Upgrade 'lein-isolate'

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :dependencies [^:source-dep [org.yaml/snakeyaml "1.17"]
                  ^:source-dep [rewrite-clj "0.6.1" :exclusions [org.clojure/clojure]]
                  ^:source-dep [cheshire "5.8.0"]]
-  :plugins [[lein-isolate "0.1.0"]]
+  :plugins [[lein-isolate "0.1.1"]]
   :middleware [leiningen.isolate/middleware]
   :eval-in :leiningen
   :pedantic? :abort)

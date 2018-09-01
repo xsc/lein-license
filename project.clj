c688cb5076a8c1221e52aff142f81e428b3fa80a
Yannick Scherer <yannick.scherer@futurice.com>
2018-09-01 13:13:57 +0200

    Version 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-license "0.1.7-SNAPSHOT"
+(defproject lein-license "0.1.7"
   :description "Project-Level License Management."
   :url "https://github.com/xsc/lein-license"
   :license {:name "MIT License"

154d24ab09fded5c5765a1da37cef599221384a5
Yannick Scherer <yannick.scherer@futurice.com>
2021-01-17 15:34:54 +0100

    Update license

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject lein-license "0.1.9-SNAPSHOT"
   :description "Project-Level License Management."
   :url "https://github.com/xsc/lein-license"
-  :license {:name "MIT License"
-            :url "https://opensource.org/licenses/MIT"
+  :license {:name "MIT"
+            :url "https://choosealicense.com/licenses/mit"
             :key "mit"
-            :year 2015}
+            :year 2015
+            :comment "MIT License"}
   :dependencies [^:source-dep [org.yaml/snakeyaml "1.17"]
                  ^:source-dep [rewrite-clj "0.6.1" :exclusions [org.clojure/clojure]]
                  ^:source-dep [cheshire "5.8.0"]]
